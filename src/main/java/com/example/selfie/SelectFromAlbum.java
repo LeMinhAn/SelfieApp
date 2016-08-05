fc43ae3eb7aa392806c9bf360a446da4109a824b
Le Minh An <an.leminh@ptmobilegroup.com>
2016-08-05 14:15:06 +0700

    Update
    Update

--- src/main/java/com/example/selfie/SelectFromAlbum.java
+++ src/main/java/com/example/selfie/SelectFromAlbum.java
@@ -1,12 +1,31 @@
 package com.example.selfie;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.example.selfie.view.BubbleInputDialog;
+import com.example.selfie.view.BubbleTextView;
+import com.example.selfie.view.StickerView;
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
+
+import java.util.ArrayList;
+
 
 public class SelectFromAlbum extends AppCompatActivity {
+    private FloatingActionsMenu mMultipleActions;
+    private View mAddSticker;
+    private View mAddBubble;
+    private ArrayList<View> mViews;
+    private BubbleInputDialog mBubbleInputDialog;
+    private StickerView mCurrentView;
+    private BubbleTextView mCurrentEditTextView;
+    private RelativeLayout mContentRootView;
 
     ImageView imageView;
     Uri uri;
@@ -16,15 +35,38 @@ public class SelectFromAlbum extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_from_album);
-
+        mContentRootView = (RelativeLayout) findViewById(R.id.rl_content_root);
         imageView = (ImageView) findViewById(R.id.imageView2);
 
         Intent intent = getIntent();
 
         uri = intent.getParcelableExtra("Pictrue");
         imageView.setImageURI(uri);
-
-
+        mMultipleActions = (FloatingActionsMenu) findViewById(R.id.multiple_actions);
+        mAddSticker = findViewById(R.id.action_add_sticker);
+        mAddBubble = findViewById(R.id.action_add_bubble);
+        mAddSticker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addStickerView();
+                mMultipleActions.collapse();
+            }
+        });
+        mAddBubble.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addBubble();
+                mMultipleActions.collapse();
+            }
+        });
+        mViews = new ArrayList<>();
+        mBubbleInputDialog = new BubbleInputDialog(this);
+        mBubbleInputDialog.setCompleteCallBack(new BubbleInputDialog.CompleteCallBack() {
+            @Override
+            public void onComplete(View bubbleTextView, String str) {
+                ((BubbleTextView) bubbleTextView).setText(str);
+            }
+        });
     }
 
     @Override
@@ -33,6 +75,101 @@ public class SelectFromAlbum extends AppCompatActivity {
         startActivity(intent);
 
     }
+    private void addStickerView() {
+        final StickerView stickerView = new StickerView(this);
+        stickerView.setImageResource(R.mipmap.ic_cat);
+        stickerView.setOperationListener(new StickerView.OperationListener() {
+            @Override
+            public void onDeleteClick() {
+                mViews.remove(stickerView);
+                mContentRootView.removeView(stickerView);
+            }
 
+            @Override
+            public void onEdit(StickerView stickerView) {
+                if (mCurrentEditTextView != null) {
+                    mCurrentEditTextView.setInEdit(false);
+                }
+                mCurrentView.setInEdit(false);
+                mCurrentView = stickerView;
+                mCurrentView.setInEdit(true);
+            }
 
+            @Override
+            public void onTop(StickerView stickerView) {
+                int position = mViews.indexOf(stickerView);
+                if (position == mViews.size() - 1) {
+                    return;
+                }
+                StickerView stickerTemp = (StickerView) mViews.remove(position);
+                mViews.add(mViews.size(), stickerTemp);
+            }
+        });
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+        mContentRootView.addView(stickerView, lp);
+        mViews.add(stickerView);
+        setCurrentEdit(stickerView);
+    }
+    private void setCurrentEdit(StickerView stickerView) {
+        if (mCurrentView != null) {
+            mCurrentView.setInEdit(false);
+        }
+        if (mCurrentEditTextView != null) {
+            mCurrentEditTextView.setInEdit(false);
+        }
+        mCurrentView = stickerView;
+        stickerView.setInEdit(true);
+    }
+    private void setCurrentEdit(BubbleTextView bubbleTextView) {
+        if (mCurrentView != null) {
+            mCurrentView.setInEdit(false);
+        }
+        if (mCurrentEditTextView != null) {
+            mCurrentEditTextView.setInEdit(false);
+        }
+        mCurrentEditTextView = bubbleTextView;
+        mCurrentEditTextView.setInEdit(true);
+    }
+    private void addBubble() {
+        final BubbleTextView bubbleTextView = new BubbleTextView(this,
+                Color.WHITE, 0);
+        bubbleTextView.setImageResource(R.mipmap.bubble_7_rb);
+        bubbleTextView.setOperationListener(new BubbleTextView.OperationListener() {
+            @Override
+            public void onDeleteClick() {
+                mViews.remove(bubbleTextView);
+                mContentRootView.removeView(bubbleTextView);
+            }
+
+            @Override
+            public void onEdit(BubbleTextView bubbleTextView) {
+                if (mCurrentView != null) {
+                    mCurrentView.setInEdit(false);
+                }
+                mCurrentEditTextView.setInEdit(false);
+                mCurrentEditTextView = bubbleTextView;
+                mCurrentEditTextView.setInEdit(true);
+            }
+
+            @Override
+            public void onClick(BubbleTextView bubbleTextView) {
+                mBubbleInputDialog.setBubbleTextView(bubbleTextView);
+                mBubbleInputDialog.show();
+            }
+
+            @Override
+            public void onTop(BubbleTextView bubbleTextView) {
+                int position = mViews.indexOf(bubbleTextView);
+                if (position == mViews.size() - 1) {
+                    return;
+                }
+                BubbleTextView textView = (BubbleTextView) mViews.remove(position);
+                mViews.add(mViews.size(), textView);
+            }
+        });
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+        mContentRootView.addView(bubbleTextView, lp);
+        mViews.add(bubbleTextView);
+        setCurrentEdit(bubbleTextView);
+    }
 }
